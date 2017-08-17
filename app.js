96967019ca1596458a97f7321eea514d06ddc5a4
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-17 15:56:48 -0400

    Checkbox takes value from todo text

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'mustache');
 //I got css to serve to website by putting styles in a css subfolder
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
 app.get('/', function(req, res){
@@ -28,8 +28,12 @@ app.post('/', function(req, res){
   res.redirect('/');
 });
 
+//I am attempting to get feedback from a check box click
+app.get('/:todoCheck', function(req, res){
+  console.log(req.body.todoCheck);
+});
+
 app.listen(port, function(){
   console.log("Listening on " + port);
-  console.log(app.set);
-  console.log(app.use);
+
 });
